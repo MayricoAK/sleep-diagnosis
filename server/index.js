32db9f8c84ee177f6d75a7986033c201cc396876
MayricoAK <mayricoak@gmail.com>
2024-08-06 18:55:26 +0700

    modify console log in index.js

--- server/index.js
+++ server/index.js
@@ -19,17 +19,18 @@ app.use('/diagnose', diagnosisRoutes);
 app.use('/auth', authRoutes);
 app.use('/user', userRoutes);
 
-mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('Connected to MongoDB'))
-  .catch(err => console.error('Could not connect to MongoDB', err));
+mongoose.connect(process.env.MONGO_URL, { 
+  useNewUrlParser: true, 
+  useUnifiedTopology: true 
+}).then(() =>{ 
+  console.log('Connected to MongoDB');
+  app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+
+  // Solution.insertMany(dataDiagnosisSolution);
+}).catch(err => console.error('Could not connect to MongoDB', err));
 
 app.get('/', (req, res) => {
   res.send(`
     <h1>ACCESS, FLASH!</h1>
   `);
-});
-
-  // Solution.insertMany(dataDiagnosisSolution);
-  app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
 });
